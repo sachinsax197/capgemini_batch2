13953e95b1441d9ae9f85d24d1e0cf9bbf4d33b6
sachinsax197 <67625616+sachinsax197@users.noreply.github.com>
2022-03-15 16:06:24 +0530

    assignment2

---
+++ aassignment2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define exp1 num%10
+#define exp2 num=num/10
+
+
+int main()
+{
+  int num=0,i=1,sum=0,mul=1;
+  printf("Enter the number=");
+  scanf("%d",&num);
+  while(i<=num)
+  {
+    if(exp1!=0)
+    {
+      sum+=exp1;
+      mul*=exp1;
+    }
+    exp3;
+  }
+  printf("\n Sum=%d\n",sum);
+  printf("\n Mul=%d\n",mul);
+  }
