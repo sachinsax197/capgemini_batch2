e732b7485c7d13cc2d3a4c15d61522fc328051d7
sachinsax197 <67625616+sachinsax197@users.noreply.github.com>
2022-03-16 09:18:32 +0530

    Update assignment6.c

--- assignment6.c
+++ assignment6.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#define exp1 num%10
+#define exp2 num=num/10
 
+
+int main()
+{
+  int num=0,i=1,even=0,odd=1;
+  printf("Enter the number=");
+  scanf("%d",&num);
+  while(i<=num)
+  {
+      even+=exp1;
+      exp2;
+    
+      odd*=exp1;
+      exp2;
+  }
+  
+  if(even%odd==0)
+    printf("\n True");
+  else
+  printf("\n True");
+  }
